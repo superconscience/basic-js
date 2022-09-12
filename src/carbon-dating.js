256d3ad2f98bf593483add92c8ea15a66b096117
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2022-09-12 23:30:15 +0300

    feat: complete the `Carbon dating` task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    sampleActivity === undefined
+    || typeof sampleActivity !== 'string'
+  ) return false;
+
+  const sampleActivityNumber = Number(sampleActivity);
+
+  if (
+    Number.isNaN(sampleActivityNumber)
+    || sampleActivityNumber <= 0
+  ) return false;
+
+  const result = Math.ceil(Math.log2(MODERN_ACTIVITY / Number(sampleActivity)) * HALF_LIFE_PERIOD);
+
+  return result > 0 ? result : false;
 }
 
 module.exports = {
