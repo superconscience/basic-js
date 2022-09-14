689c7d4d37be59419c712b72a2275169d2c3663d
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2022-09-15 01:20:53 +0300

    feat: complete the task `Vigenere cipher`

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  __ERROR_INVALID_ARGUMENTS__ = 'Incorrect arguments!'
+  constructor(direct = true) {
+    this._direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(...args) {
+    return this._do(
+      (message, key) => {
+        let encryptedMessage = '';
+        for (let i = 0, k = 0; i < message.length; i++) {
+          const char = message[i];
+          if (!ALPHABET.includes(char)) {
+            encryptedMessage += char;
+            continue;
+          }
+          encryptedMessage += VIGENERE_TABLE[char][key[k]];
+          k++;
+        }
+        return encryptedMessage;
+      },
+      ...args
+    );
+  }
+  decrypt(...args) {
+    return this._do(
+      (message, key) => {
+        let encryptedMessage = '';
+        for (let i = 0, k = 0; i < message.length; i++) {
+          const char = message[i];
+          if (!ALPHABET.includes(char)) {
+            encryptedMessage += char;
+            continue;
+          }
+          encryptedMessage += Object.entries(VIGENERE_TABLE[key[k]])
+            .find(([columnKey, columnValue]) => columnValue === char)[0];
+          k++;
+        }
+        return encryptedMessage;
+      },
+      ...args
+    );
+  }
+  _do(handleMessage, ...args) {
+    if (args.length < 2 || args[0] === undefined || args[1] === undefined)
+      throw new Error(this.__ERROR_INVALID_ARGUMENTS__);
+
+    let [message, key] = this._prepare(...args);
+
+    if (key === '')
+      return message;
+
+    let handledMessage = handleMessage(message, key);
+
+    return this._direct
+      ? handledMessage
+      : handledMessage.split('').reverse().join('');
+  }
+  _prepare(message, key) {
+    message = String(message);
+    key = String(key);
+
+    message = message.toUpperCase();
+    key = key.toUpperCase()
+      .repeat(Math.ceil(message.length / key.length))
+      .slice(0, message.length);
+    return [message, key];
   }
 }
+
+const ALPHABET = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+const VIGENERE_TABLE = Object.fromEntries(
+  ALPHABET.map((char, i) => [
+    char,
+    Object.fromEntries(
+      [
+        ...ALPHABET.slice(i),
+        ...ALPHABET.slice(0, i)
+      ].map((char, k) => [ALPHABET[k], char])
+    )
+  ])
+);
 
 module.exports = {
   VigenereCipheringMachine
